896e37ae9b9e0c343d6af4645e439aefd97827e6
CristinaAI22 <cristina.ionica22@gmail.com>
2023-06-11 21:18:06 +0300

    tema 11

--- src/index.js
+++ src/index.js
@@ -1,73 +1,141 @@
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
 const axios = require('axios').default;
-const form = document.querySelector('.search-form');
-const search = document.getElementsByName('searchQuery')[0];
-const imageGallery = document.querySelector('.gallery');
+const lightbox = new SimpleLightbox('.gallery a');
+const searchForm = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreButton = document.querySelector('.load-more');
+const apiKey = '37150177-120515d24bc5803fc768400f4';
+const baseUrl = 'https://pixabay.com/api/';
 
-form.addEventListener('submit', function (e) {
+let itemsPerPage = 40;
+let currentPage = 1;
+let initialQuery = '';
+
+searchForm.addEventListener('submit', async function (e) {
   e.preventDefault();
-  const searchInput = search.value;
-  searchImages(searchInput);
+  initialQuery = searchForm.elements.searchQuery.value;
+  currentPage = 1;
+  clearGallery();
+  await searchImages(initialQuery, currentPage, itemsPerPage);
 });
-async function searchImages(searchInput) {
-  const apiKey = '37150177-120515d24bc5803fc768400f4';
+
+loadMoreButton.addEventListener('click', async function (e) {
+  e.preventDefault();
+  currentPage++;
+  const totalItems = itemsPerPage * currentPage;
+  await searchImages(initialQuery, currentPage, totalItems);
+});
+
+async function searchImages(query, page, perPage) {
+  showLoader();
   try {
-    const response = await axios.get('https://pixabay.com/api/', {
+    const response = await axios.get(baseUrl, {
       params: {
         key: apiKey,
-        q: searchInput,
+        q: query,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+        page: page,
+        per_page: perPage,
       },
     });
-    const images = response.data.hits;
+    const { hits, totalHits } = response.data;
 
-    if (images.length === 0) {
-      Notiflix.Notify.warning(
+    if (hits.length === 0 || query === '') {
+      showNotification(
         'Sorry, there are no images matching your search query. Please try again.'
       );
     } else {
-      displayImages(images);
+      addImagesToGallery(hits);
+
+      if (hits.length < totalHits) {
+        showLoadMoreButton();
+        showNotification(`Hooray! We found ${totalHits} images.`);
+      } else {
+        hideLoadMoreButton();
+        showNotification(`Hooray! We found ${totalHits} images.`);
+      }
     }
   } catch (error) {
     Notiflix.Notify.failure(
-      'An error occurred while fetching images. Please try again later.'
+      'An error occurred while fetching the images. Please try again later.'
     );
+  } finally {
+    hideLoader();
   }
 }
-function displayImages(images) {
-  imageGallery.innerHTML = '';
 
-  images.forEach(function (image) {
-    const photoCard = document.createElement('div');
-    photoCard.classList.add('photo-card');
+function addImagesToGallery(images) {
+  const cards = images.map(image => createImageCard(image));
+  gallery.innerHTML = '';
+  cards.forEach(card => {
+    gallery.appendChild(card);
+  });
+  lightbox.refresh();
+  smoothScroll();
+}
 
-    const imageElement = document.createElement('img');
-    imageElement.src = image.webformatURL;
-    imageElement.alt = image.tags;
-    imageElement.loading = 'lazy';
-    const infoContainer = document.createElement('div');
-    infoContainer.classList.add('info');
-    const likes = document.createElement('p');
-    likes.classList.add('info-item');
-    likes.innerHTML = `<b>Likes:</b> ${image.likes}`;
-    const views = document.createElement('p');
-    views.classList.add('info-item');
-    views.innerHTML = `<b>Views:</b> ${image.views}`;
-    const comments = document.createElement('p');
-    comments.classList.add('info-item');
-    comments.innerHTML = `<b>Comments:</b> ${image.comments}`;
-    const downloads = document.createElement('p');
-    downloads.classList.add('info-item');
-    downloads.innerHTML = `<b>Downloads:</b> ${image.downloads}`;
+function createImageCard(image) {
+  const card = document.createElement('div');
+  card.classList.add('photo-card');
 
-    infoContainer.appendChild(likes);
-    infoContainer.appendChild(views);
-    infoContainer.appendChild(comments);
-    infoContainer.appendChild(downloads);
-    photoCard.appendChild(image);
-    photoCard.appendChild(infoContainer);
-    imageGallery.appendChild(card);
+  const imageLink = document.createElement('a');
+  imageLink.href = image.largeImageURL;
+  imageLink.setAttribute('data-lightbox', 'gallery');
+  imageLink.setAttribute('data-title', image.tags);
+
+  const img = document.createElement('img');
+  img.src = image.webformatURL;
+  img.alt = image.tags;
+  img.loading = 'lazy';
+
+  const info = document.createElement('div');
+  info.classList.add('info');
+
+  const likes = createInfoItem('Likes: ', image.likes);
+  const views = createInfoItem('Views: ', image.views);
+  const comments = createInfoItem('Comments: ', image.comments);
+  const downloads = createInfoItem('Downloads: ', image.downloads);
+
+  info.append(likes, views, comments, downloads);
+  imageLink.appendChild(img);
+  card.append(imageLink, info);
+
+  return card;
+}
+
+function createInfoItem(label, value) {
+  const p = document.createElement('p');
+  p.classList.add('info-item');
+  p.innerHTML = `<b>${label}</b>${value}`;
+  return p;
+}
+function clearGallery() {
+  gallery.innerHTML = '';
+}
+function showLoadMoreButton() {
+  loadMoreButton.style.display = 'block';
+}
+function hideLoadMoreButton() {
+  loadMoreButton.style.display = 'none';
+}
+function showNotification(message) {
+  Notiflix.Notify.info(message);
+}
+function showLoader() {
+  Notiflix.Loading.standard('Loading...');
+}
+function hideLoader() {
+  Notiflix.Loading.remove();
+}
+function smoothScroll() {
+  const { height: cardHeight } =
+    gallery.firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * (currentPage - 1),
+    behavior: 'smooth',
+    block: 'start',
   });
 }
